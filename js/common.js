8f84e5d562a80565abc3b80c9ada0522ac9d5d41
dpismenny <denis.pismenny@gmail.com>
2013-02-12 17:37:28 +0200

    table

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 
+	$('.entry__type input:checked').parent().parent().parent('.entry').addClass('selected');
+	$('.entry__type input').click(function(){
+		 if (this.checked){
+			 $(this).parent().parent().parent('.entry').addClass('selected');
+		 }
+		else{
+			$(this).parent().parent().parent('.entry').removeClass('selected');
+		};
+	});
 });
