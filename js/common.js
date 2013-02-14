7f0244b390c8e806dc9b36cc6124e847a6fc6c14
akella <akella.a@gmail.com>
2013-02-14 16:01:33 +0200

    up js

--- js/common.js
+++ js/common.js
@@ -11,11 +11,46 @@ $(document).ready(function() {
 	});
 
 	$('.movie__pic').click(function(event) {
-		$('.movie_current').removeClass('movie_current');
-		$(this).toggleClass('flipped');
-		$('body').toggleClass('movie_fade');
-		$(this).parent().addClass('movie_current');
+		if($(this).parent().hasClass('movie_current')){
+			$(this).parent().removeClass('movie_current');
+			$('body').removeClass('movie_fade');
+			$(this).toggleClass('flipped');
+		}
+		else{
+			$('.movie_current').children('.flipped').removeClass('flipped');
+			$('.movie_current').removeClass('movie_current');
+
+			$(this).toggleClass('flipped');
+			$('body').addClass('movie_fade');
+			$(this).parent().addClass('movie_current');
+		}
+		
+
 	});
 
+	$('.movie__more').click(function(event) {
+		$(this).hide();
+		$(this).next().show();
+		// getting place of details
+		number = $(this).parents('.movie').index() + 1;
+		row = parseInt($(window).width()/287);
+		numberinrow = number%row;
+		if(numberinrow==0){numberinrow=4;}
+		rownumber = parseInt(number/row);
+		if(rownumber==0){rownumber=1;}
+
+		//alert(number+'number of block---'+row+'inarow---'+numberinrow+'ninaro=='+rownumber+'FINALL'+row*rownumber);
+		// getting place of arr for details
+		$('.details__arr').css('left', 287*numberinrow - 120);
+		//moving details where it should
+		$det = $('.details');
+		paren = $('.movie:nth-child('+row*rownumber+')');
+		$det.insertAfter(paren);
+		$('.details').toggle();
+
+	});
+	$('.details__close,.movie__less').click(function(event) {
+		$(this).closest('.details').toggle();
+	});
 
 });
